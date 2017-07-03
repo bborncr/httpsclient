4bf406e7530681a5aa1945e8ed282bfab345d533
bborncr <bentley@crcibernetica.com>
2017-07-03 17:28:54 +0000

    added 3 different callback examples

---
+++ callbacks/print_uppercase.js
@@ -0,0 +1,13 @@
+var getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/uppercase.html'
+};
+
+function printUpperCase (html) {
+  var htmlUpperCase = html.toUpperCase();
+  console.log(htmlUpperCase);
+}
+
+getHTML(requestOptions, printUpperCase);
